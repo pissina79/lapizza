1bbd88390bb4039c77d65c0962f542433a63a4a3
Paris Lia Hyun <parisliahyun@gmail.com>
2013-06-30 11:42:02 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lapizza::Application.config.secret_token = '7ff5259eb7c439f6c9ac90bdd75f3a8e3054259ef6092c281364549ace4e76614c0f81b9ff34c134f15fb35031cc7412883cd41cb9118d4099a75c09078d0364'
